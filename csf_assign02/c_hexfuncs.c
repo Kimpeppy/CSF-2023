85ff1f1df8c0307f06d6efbd0f69209f238bb841
Afaf Saqib Qureshi <asaqibq1@jhu.edu>
2023-02-28 20:18:43 -0500

    finally fixed main lol

--- csf_assign02/c_hexfuncs.c
+++ csf_assign02/c_hexfuncs.c
@@ -1,21 +1,23 @@
 // C implementation of hexdump functions
 
-#include <unistd.h>  // this is the only system header file you may include!
+#include <unistd.h> // this is the only system header file you may include!
 #include "hexfuncs.h"
-
 
 // Read up to 16 bytes from standard input into data_buf.
 // Returns the number of characters read.
-unsigned hex_read(char data_buf[]) {
-//STDIN_FILENO
+unsigned hex_read(char data_buf[])
+{
+    // STDIN_FILENO
     unsigned byte = read(0, data_buf, 16);
     return byte;
 }
 
 // Write given nul-terminated string to standard output.
-void hex_write_string(const char s[]) {
+void hex_write_string(const char s[])
+{
     unsigned length = 0;
-    while (s[length] != '\0') {
+    while (s[length] != '\0')
+    {
         length++;
     }
     write(1, s, length);
@@ -24,60 +26,71 @@ void hex_write_string(const char s[]) {
 // Format an unsigned value as an offset string consisting of exactly 8
 // hex digits.  The formatted offset is stored in sbuf, which must
 // have enough room for a string of length 8.
-void hex_format_offset(unsigned offset, char sbuf[]) {
+void hex_format_offset(unsigned offset, char sbuf[])
+{
     int temp;
     sbuf[8] = '\0';
     int pointer = 7;
-    while (offset > 0) {
+    while (offset > 0)
+    {
         temp = offset & 15;
-        if (temp >= 0 && temp < 10) {
+        if (temp >= 0 && temp < 10)
+        {
             sbuf[pointer] = temp + 48;
         }
-        else {
+        else
+        {
             sbuf[pointer] = temp + 87;
         }
         offset = offset >> 4;
         pointer--;
     }
-    while(pointer >= 0) {
+    while (pointer >= 0)
+    {
         sbuf[pointer] = '0';
         pointer--;
     }
-
 }
 
 // Format a byte value (in the range 0-255) as string consisting
 // of two hex digits.  The string is stored in sbuf.
-void hex_format_byte_as_hex(unsigned char byteval, char sbuf[]) {
+void hex_format_byte_as_hex(unsigned char byteval, char sbuf[])
+{
     char first;
     char second;
     sbuf[2] = '\0';
     first = byteval >> 4;
     second = byteval & 15;
-    if (first >= 0 && first < 10) {
+    if (first >= 0 && first < 10)
+    {
         sbuf[0] = first + 48;
-    } 
-    else {
+    }
+    else
+    {
         sbuf[0] = first + 87;
     }
-    if (second >= 0 && second < 10) {
+    if (second >= 0 && second < 10)
+    {
         sbuf[1] = second + 48;
-    } 
-    else {
+    }
+    else
+    {
         sbuf[1] = second + 87;
     }
-
 }
 
 // Convert a byte value (in the range 0-255) to a printable character
 // value.  If byteval is already a printable character, it is returned
 // unmodified.  If byteval is not a printable character, then the
 // ASCII code for '.' should be returned.
-char hex_to_printable(unsigned char byteval) {
-    if (byteval > 31 && byteval < 127) {
+char hex_to_printable(unsigned char byteval)
+{
+    if (byteval > 31 && byteval < 127)
+    {
         return byteval;
-    } 
-    else {
+    }
+    else
+    {
         return '.';
     }
 }
--- csf_assign02/c_hexmain.c
+++ csf_assign02/c_hexmain.c
@@ -1,17 +1,18 @@
 
 // C implementation of hexdump main function
 
-#include "hexfuncs.h"  // this is the only header file which may be included!
+#include "hexfuncs.h" // this is the only header file which may be included!
 
 void helper_call_format_as_hex(char data_buf[], char sbuf[], unsigned temp);
 
-int main(void) {
-  
+int main(void)
+{
+
   char data_buf[17];
   char sbuf[9];
   data_buf[16] = '\0';
 
-  //Reference
+  // Reference
   char colon[2] = {':', '\0'};
   char space[2] = {' ', '\0'};
   char newLine[2] = {'\n', '\0'};
@@ -20,7 +21,8 @@ int main(void) {
   unsigned temp = hex_read(data_buf);
   data_buf[temp] = '\0';
   unsigned counter = 0;
-  while (temp != 0) {
+  while (temp != 0)
+  {
     // Get the first line of position and write it
     hex_format_offset(counter, sbuf);
     hex_write_string(sbuf);
@@ -35,19 +37,23 @@ int main(void) {
     data_buf[temp] = '\0';
     counter += 16;
   }
-  
 }
 
-void helper_call_format_as_hex(char data_buf[], char sbuf[], unsigned temp) {
-  char space[2] = {' ', '\0'};
-  for (unsigned i = 0; i < temp; i++) {
-    hex_format_byte_as_hex(data_buf[i], sbuf);
-    hex_write_string(sbuf);
-    data_buf[i] = hex_to_printable(data_buf[i]);
-    hex_write_string(space);
+void helper_call_format_as_hex(char data_buf[], char sbuf[], unsigned temp)
+{
+  char space[2] = {' ', '\0'}; // space
+
+  for (unsigned i = 0; i < temp; i++)
+  {                                              // Loop through the amount of char read
+    hex_format_byte_as_hex(data_buf[i], sbuf);   // Get the char i and convert it to hexadecimal
+    hex_write_string(sbuf);                      // Write the hexadecimal
+    data_buf[i] = hex_to_printable(data_buf[i]); // Replace the character with a "." if its not printable
+    hex_write_string(space);                     // Print space
   }
   unsigned i = temp;
-  while (i < 16) {
+  // Print out only space if there isn't enough characters
+  while (i < 16)
+  {
     hex_write_string(space);
     hex_write_string(space);
     hex_write_string(space);
