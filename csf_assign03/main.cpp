72c1a7067c0fd3ec01cbf6643de68e40c8643732
Michael <you@somewhere.com>
2023-03-06 17:57:34 -0500

    started hw3

---
+++ csf_assign03/main.cpp
@@ -0,0 +1,14 @@
+#include "csimfuncs.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+    std :: cout<<"Invalid arguments" << std::endl;
+    return 1;
+  }
+}
