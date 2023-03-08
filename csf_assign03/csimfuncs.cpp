9d14def7696c48be55711379e926a91465530179
Michael <you@somewhere.com>
2023-03-08 16:52:55 -0500

    fds

---
+++ csf_assign03/csimfuncs.cpp
@@ -0,0 +1,12 @@
+#include "csimfuncs.h"
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+CacheClass:: CacheClass() {
+    
+}
--- csf_assign03/csimfuncs.h
+++ csf_assign03/csimfuncs.h
@@ -1,18 +1,25 @@
-#ifdef CSIMFUNCS_H
+#ifndef CSIMFUNCS_H
 #define CSIMFUNCS_H
 
-struct Slot {
-    uint32_t tag;
-    bool valid;
-    uint32_t load_ts, access_ts;
+class CacheClass {
+public:
+    // Default constructor
+    CacheClass();
+
+private:
+    struct Slot {
+        uint32_t tag;
+        bool valid;
+        uint32_t load_ts, access_ts;
+    };
+
+    struct Set {
+        std::vector<Slot> slots;
+    };
+
+    struct Cache{
+        std::vector<Set> sets;
+    };
 };
 
-struct Set {
-    std::vector<Slot> slots;
-};
-
-struct Cache {
-    std::vector<Set> sets;
-};
-
-#endif // CSIMFUNC_H
+#endif
