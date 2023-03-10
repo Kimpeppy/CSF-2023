0105f203e0139fde28b4ed26fc42c9c1d54310b2
Michael <you@somewhere.com>
2023-03-10 18:05:58 -0500

    made changes

--- csf_assign03/csimfuncs.cpp
+++ csf_assign03/csimfuncs.cpp
@@ -6,7 +6,72 @@
 #include <vector>
 #include <map>
 #include <algorithm>
+#include <limits.h>
+#include <unordered_map>
 
-CacheClass:: CacheClass() {
-    
+
+
+Cache* createEmptyCache(long cycle, long numOfBytes, long numOfSets, long numOfBlocks) {
+    Cache newCache;
+    // Initalize the sets in cache
+    for (long i = 0; i < numOfSets; i++) {
+        Set newSet;
+        // Initalize the slots in sets
+        for (long j = 0; j < numOfBlocks; j++) {
+            Slot newSlot = {j, false, false, 0, 0};
+            newSet.slots.push_back(newSlot);
+        }
+        newCache.sets.push_back(newSet);
+    }
+    return &newCache;
 }
+
+void load_to_cache(Cache mainCache, Slot replacement, long numOfBytes, long newTag) {
+    mainCache.cycle += (numOfBytes/4) * 100;
+    // Find the slots of the sets
+    // Punish for dirty bit
+    if (replacement.dirty) {
+        mainCache.cycle += (numOfBytes/4) * 100;
+    }
+    if (!(replacement.valid)) {
+        replacement.valid = true;
+    }
+    replacement.tag = newTag;
+    replacement.load_ts = mainCache.cycle;
+    replacement.access_ts = mainCache.cycle;
+
+}
+
+Slot block_to_replace(std::vector<Slot> slot, Cache mainCache, bool lru, long tag) {
+    // Do a linear search to find where the valid index is
+    // Break if you find a valid index
+    // Track lowest load_ts and highest access_ts
+    long longest_load_ts = 0;
+    long longest_access_ts = 0;
+
+    Slot llt;
+    Slot lat;
+
+    
+    for (Slot& it: slot) {
+        if (!(it.valid)) {
+            return it;
+        }
+        if (it.load_ts > longest_load_ts) {
+            longest_load_ts = it.load_ts;
+            llt = it;
+        }
+        if (it.access_ts > longest_access_ts) {
+            longest_access_ts = it.access_ts;
+            lat = it;
+        }
+    }
+    if (lru) {
+        return llt;
+    } else {
+        return lat;
+    }
+
+
+}
+
--- csf_assign03/csimfuncs.h
+++ csf_assign03/csimfuncs.h
@@ -1,16 +1,14 @@
 #ifndef CSIMFUNCS_H
 #define CSIMFUNCS_H
+#include <vector>
+#include <map>
+#include <unordered_map>
 
-class CacheClass {
-public:
-    // Default constructor
-    CacheClass();
-
-private:
     struct Slot {
-        uint32_t tag;
+        long tag;
         bool valid;
-        uint32_t load_ts, access_ts;
+        bool dirty;
+        long load_ts, access_ts;
     };
 
     struct Set {
@@ -19,7 +17,20 @@ private:
 
     struct Cache{
         std::vector<Set> sets;
+        long cycle; // Timer
     };
-};
+
+    // Create an empty cache and initalize it with empty values
+    // It will process the parameter to make enough room for the cache 
+    Cache createEmptyCache(long numOfBytes, long numOfSets, long numOfBlocks);
+    
+    // This function loads from memory to the cache
+    // This function will only accept misses
+    // Update the cache cycle
+    void load_to_cache(Cache mainCache, Slot replacement, long numOfBytes, long newTag);
+
+    // Find out which slot needs to be replace or find the slot that was hit
+    // Returns either an invalid slot or LRU or FIFO slot
+    Slot block_to_replace(std::vector<Slot> slot, Cache mainCache, bool lru);
 
 #endif
