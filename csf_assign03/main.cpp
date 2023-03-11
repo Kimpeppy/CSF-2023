f411e29b1328b699aa4d0b9c448acbb98a95dfbb
Afaf Saqib Qureshi <asaqibq1@jhu.edu>
2023-03-10 19:04:42 -0500

    working on main

--- csf_assign03/main.cpp
+++ csf_assign03/main.cpp
@@ -7,33 +7,45 @@
 #include <map>
 #include <algorithm>
 
-struct Slot {
-    uint32_t tag;
-    bool valid;
-    uint32_t load_ts, access_ts;
-};
+using std::cin;
+using std::stoi
+using std::cout;
+using std::endl;
+using std::ifstream;
+using std::map;
+using std::string;
+using std::vector;
 
-struct Set {
-    std::vector<Slot> slots;
-};
 
-struct Cache {
-    std::vector<Set> sets;
-};
-
-// Note
-// Use STDHEX to change the hex to a string
-int main(int argc, char* argv[]) {
-    if (argc != 4) {
-        std :: cout<<"Invalid arguments" << std::endl;
+int main(int argc, char *argv[])
+{
+    // if number of arguments is not correct, exit with code 1
+    if (argc < 6)
+    {
+        cout << "Invalid arguments";
         return 1;
     }
-    std::istringstream converter { argv[2] };
 
-    long address = 0;
-    converter >> std::hex >> address;
-
-    std :: cout << address << std::endl;
-
+    char first_arg;
+    uint32_t address;
+    char garbage;
+    // Convert arguments to integer values
+    int numBlockBytes = stoi(arg[2]);
+    int numSets = stoi(arg[0]);
+    int numBlocks = stoi(arg[1]);
     
+    // Create empty cache with input arguments
+    createEmptyCache(numBlockBytes, numSets, numBlocks);
+    uint32_t index;
+    uint32_t offset;
+    index = log2(numSets);
+    offset = log2(numBlockBytes);
+    
+    // Read in chained data from standard input
+    // Use std::hex to convert the string address to hex
+    while (cin >> first_arg >> std::hex >> address >> garbage) 
+    {
+           
+    }
+
 }
